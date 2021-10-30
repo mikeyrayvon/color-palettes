66dd2bb48dee7efe0e5187b1af6e06365452f9c4
mikeyrayvon <mikey@interglobal.vision>
2021-10-30 18:23:28 +0200

    add tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
