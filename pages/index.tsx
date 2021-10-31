5cbd3fb92d0d40252e58dc585dbda0a45684e4f6
mikeyrayvon <mikey@interglobal.vision>
2021-10-31 14:25:39 +0100

    remove getStaticProps default functionality. add colorpickers and newcolor components. add update functions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,65 @@
-import Link from 'next/link'
-
 import type { NextPage } from 'next'
-
+import { useStore } from '../utils/store'
 import Layout from '../components/Layout'
 import Container from '../components/Container'
+import { Color } from '../utils/types'
+import ColorPicker from '../components/ColorPicker'
+import NewColor from '../components/NewColor'
+import { initialColor } from '../utils/constants'
+import { hexToRGB } from '../utils/tools'
+import { postData } from '../utils/api'
 
 const Landing: NextPage = () => {
+  const { state, dispatch } = useStore()
+
+  const addColor = (): void => {
+    dispatch({type: 'add color', payload: {
+      ...initialColor,
+      order: state.colors.length + 1
+    }})
+  }
+
+  const updateColor = async (color: Color, hex: string) => {
+    const rgb: number[] | boolean = hexToRGB(hex.slice(1))
+    const updated: Color = {
+      ...color,
+      name: '...',
+      hex,
+      rgb: rgb ? rgb.toString() : ''
+    }
+    dispatch({type: 'update color', payload: updated})
+  }
+
+  const updateName = async (color: Color) => {
+    const colorName = await postData('api/getColorName', {
+      hex: color.hex.slice(1)
+    })
+    const updated: Color = {
+      ...color,
+      name: colorName.name ? colorName.name : 'No name',
+    }
+    dispatch({type: 'update color', payload: updated})
+  }
+
   return (
     <Layout>
       <Container>
-        
+        <div className='flex flex-wrap'>
+          {state?.colors.map(color => <ColorPicker 
+            color={color} 
+            updateColor={updateColor} 
+            updateName={updateName}
+            />)}
+          <NewColor addColor={addColor} />
+        </div>
       </Container>
     </Layout>
   )
 }
 
 export const getStaticProps = async () => {
-  const docs = await getClient(true).fetch(query)
-  const config = await getClient().fetch(configQuery)
   return {
-    props: { docs, config } // will be passed to the page component as props
+    props: {}
   }
 }
 
