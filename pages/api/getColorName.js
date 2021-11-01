015f64b81c279578eaebc5d3859499dcc42ed060
mikeyrayvon <mikey@interglobal.vision>
2021-11-01 22:01:08 +0100

    remove node-fetch

--- pages/api/getColorName.js
+++ pages/api/getColorName.js
@@ -1,6 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import fetch from 'node-fetch'
-
 const ENDPOINT = 'https://colornames.org/search/json/?hex='
 
 export default async (
