4cc8624d1202dfd8001276abf998f4699ed66178
mikeyrayvon <mikey@interglobal.vision>
2021-11-02 10:45:33 +0100

    restore getName

--- pages/api/getName.ts
+++ pages/api/getName.ts
@@ -2,14 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  res.json({'hexCode': '000000', 'name': 'hello'})
-  res.status(200).end()
-  /*try {
+  try {
     const response = await axios.get('https://colornames.org/search/json/?hex=' + req.body.hex)
     res.json(response.data)
     res.status(200).end()
   } catch (error) {
     res.json(error)
     res.status(405).end()
-  }*/
+  }
 }
