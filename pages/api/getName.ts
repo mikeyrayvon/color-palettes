312fb0f415607e15f8bb3b158d06d682626a98da
mikeyrayvon <mikey@interglobal.vision>
2021-11-02 17:50:15 +0100

    change http method

--- pages/api/getName.ts
+++ pages/api/getName.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const response = await fetch('https://colornames.org/search/json/?hex=' + req.body.hex, {
-      method: 'post',
+      method: 'get',
       headers: {'Content-Type': 'application/json'}
     })
     const data = await response.json()
