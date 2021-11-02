f1716a3c486c6efd5f060eafe5f70b5a88866d5f
mikeyrayvon <mikey@interglobal.vision>
2021-11-02 14:41:19 +0100

    downgrade node-fetch. remove cross-fetch

--- pages/api/getName.ts
+++ pages/api/getName.ts
@@ -1,13 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const response = await axios.get('https://colornames.org/search/json/?hex=' + req.body.hex)
-    res.json(response.data)
-    res.status(200).end()
+    const response = await fetch('https://colornames.org/search/json/?hex=' + req.body.hex)
+    const data = await response.json()
+    res.json(data)
+    res.status(200)
   } catch (error) {
     res.json(error)
-    res.status(405).end()
+    res.status(405)
   }
 }
